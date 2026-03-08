1b26095d4c65772d85b268afb998cc842ef21e67
vuhaidanggmod565 <gmod565@gmail.com>
2026-03-08 14:20:09 +0700

    feat(repo): add JpaSpecificationExecutor, findWithDetailsById, findByType_Name, findByRole to repositories

--- src/main/java/com/studyhub/repository/UserRepository.java
+++ src/main/java/com/studyhub/repository/UserRepository.java
@@ -6,6 +6,7 @@ import com.studyhub.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
@@ -27,4 +28,6 @@ public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificat
     long countByRole(UserRole role);
 
     long countByStatus(UserStatus status);
+
+    List<User> findByRole(UserRole role);
 }
